8bbd5958d92c3687c582b102ef8f45e2620c9814
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-10-24 18:13:39 -0400

    created file structure and filled in server.js, some of some views, and part of config.js

---
+++ db/config.js
@@ -0,0 +1,7 @@
+const options = {
+  query: (e) => {
+    console.log(e.query);
+  }
+}
+
+const pg = require ('pg-promise');
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require("path");
+const methodOverride= require("method-override")
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.static(path.join((__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req,res)=> {
+  res.render('index');
+})
+
+app.listen(port, () => {
+  console.log('time to get stuff done');
+})
+
+app.use("*", (req,res) => {
+  res.status(404).send('file not found!!')
+})
+
+
+//
+// const = require("");
+// const = require("");
+// const = require("");
+// const = require("");
+// const = require("");
