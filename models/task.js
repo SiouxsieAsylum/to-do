18db703cf077962f833f475cf20f949673bda1d1
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-10-25 14:17:21 -0400

    task model done

--- models/task.js
+++ models/task.js
@@ -1,4 +1,28 @@
 const db = require('../db/config');
 const Task = {};
 
+Task.create = (task) => {
+  return db.one(`INSERT INTO tasks (title, category, description) VALUES ($1,$2)`[task.title, task.category, task.description]);
+}
+
+Task.findByCategory = (category) => {
+  return db.one(`SELECT * FROM tasks WHERE category = $1 RETURNING *`,[category])
+}
+
+Task.findByStatus = (status) => {
+  return db.one(`SELECT * FROM tasks WHERE status = $1 RETURNING *`,[status])
+}
+
+Task.findById = (id) => {
+  return db.one(`SELECT * FROM tasks WHERE id = $1 RETURNING *`,[id])
+}
+
+Task.update = (task, id) => {
+  return db.one(`UPDATE tasks SET title = $1, category = $2, description = $3 RETURNING *`[task.title, task.category, task.description]);
+}
+
+Task.delete = (id) => {
+  return db.none(`DELETE FROM tasks WHERE id = $1`,[id])
+}
+
 module.exports = Task;
