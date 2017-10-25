c419c9631349c6f9d2781f63795d8ef2b8493785
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-10-24 22:48:38 -0400

    config.js is done, migration is done, and seed is done

---
+++ db/seeds/seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (username,password) VALUES ('Andrea_McK','chantal');
+INSERT INTO users (username,password) VALUES ('APagan','miggy123');
+
+
+INSERT INTO tasks (title,category,user_id) VALUES ('Do Dishes','chores',1);
+INSERT INTO tasks (title,category,user_id) VALUES ('groceries','chores',1);
+INSERT INTO tasks (title,category,user_id) VALUES ('meeting at 5pm with Walter and Amy', 'Work',1);
+INSERT INTO tasks (title,category,user_id) VALUES ('Beccas play','childcare',1);
+INSERT INTO tasks (title,category,user_id) VALUES ('drinks at Mos Eisley','scum/villany'1);
+
+INSERT INTO tasks (title,category,user_id) VALUES ('Eat Lasagna','cooking',2);
+INSERT INTO tasks (title,category,user_id) VALUES ('Band Practice at 5pm with Walter and Amy ','Work',2);
+INSERT INTO tasks (title,category,user_id) VALUES ('Pay Bills','chores',2);
+INSERT INTO tasks (title,category,user_id) VALUES ('Pick up Apollo from the vet','childcare',2);
+INSERT INTO tasks (title,category,user_id) VALUES ('Shooting first','Han',2);
